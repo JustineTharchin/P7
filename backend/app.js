eb9e47ec196ec073b946fd634b54ebe22fd2b276
JustineTharchin <54324028+JustineTharchin@users.noreply.github.com>
2022-01-11 19:52:22 +0100

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { Sequelize } = require('sequelize');
+const path =  require('path');
+require("dotenv").config();
+
+const postRoutes = require('./routes/post');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+const db = require("./models");
+db.sequelize.sync();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', userRoutes);
+app.use('/api/post', postRoutes);
+
+module.exports = app;
+
