c57372346047c6d022c2397c6ac15cf4372fd328
JustineTharchin <54324028+JustineTharchin@users.noreply.github.com>
2022-01-18 18:32:20 +0100

    Add files via upload

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -86,3 +86,34 @@ exports.login = (req, res, next) => {
       })
       .catch(error => res.status(500).json({ error }));
 };
+
+exports.getOneUser = (req, res, next) => {
+  User.findOne ({ where: { id: req.params.id }})
+      .then((user) => res.status(200).json(user))
+      .catch(error => res.status(404).json({ error }));
+};
+
+exports.getAllUsers = (req, res, next) => {
+  User.findAll({attributes: ['id', 'email','prenom','nom']}) 
+      .then((users) => res.status(200).json(users))
+      .catch(error => res.status(400).json({ error }));
+};
+
+
+exports.updateUser = (req, res, next) => {
+  try {
+    User.update({
+        email: req.body.email
+    }, {
+        where: {
+            id: (req.params.id)
+        }
+    });
+
+    return res.status(200).send({
+        message: "email modifiée"
+    })
+} catch (err) {
+    return res.status(500).json(err);
+}
+}
