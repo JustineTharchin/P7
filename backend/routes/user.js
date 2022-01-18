fd63b52485a842565523860a7aa2e6df11807cab
JustineTharchin <54324028+JustineTharchin@users.noreply.github.com>
2022-01-18 18:32:38 +0100

    Add files via upload

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+//const auth = require('../middleware/auth');
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
+router.get('/:id', userCtrl.getOneUser);
+router.get('/', userCtrl.getAllUsers);
+router.put('/:id', userCtrl.updateUser);
 
 module.exports = router;
